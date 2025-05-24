4410202ed2c9d4fc868dbececa9554afe8faa888
dprodan <dumitru.prodan@cyphertax.net>
2025-05-24 14:08:10 +0300

    resolve conflicts

--- internal/user/repository.go
+++ internal/user/repository.go
@@ -5,7 +5,7 @@ import (
 )
 
 func GetAll(db *sql.DB) ([]User, error) {
-	rows, err := db.Query(`SELECT id, first_name, last_name, age, car_id FROM data.users`)
+	rows, err := db.Query(`SELECT id, first_name, last_name, age FROM data.users`)
 	if err != nil {
 		return nil, err
 	}
@@ -14,7 +14,7 @@ func GetAll(db *sql.DB) ([]User, error) {
 	var users []User
 	for rows.Next() {
 		var user User
-		if err := rows.Scan(&user.ID, &user.FirstName, &user.LastName, &user.Age, &user.CarID); err != nil {
+		if err := rows.Scan(&user.ID, &user.FirstName, &user.LastName, &user.Age); err != nil {
 			return nil, err
 		}
 		users = append(users, user)
@@ -23,12 +23,23 @@ func GetAll(db *sql.DB) ([]User, error) {
 	return users, nil
 }
 
-func Insert(db *sql.DB, user User) (int, error) {
+func Get(db *sql.DB, id string) (*User, error) {
+	row := db.QueryRow(`SELECT id, first_name, last_name, age FROM data.users where id = $1`, id)
+
+	var user User
+	if err := row.Scan(&user.ID, &user.FirstName, &user.LastName, &user.Age); err != nil {
+		return nil, err
+	}
+
+	return &user, nil
+}
+
+func Insert(db *sql.DB, user CreateUserRequest) (int, error) {
 	var id int
 	err := db.QueryRow(`
-        INSERT INTO data.users (first_name, last_name, age, car_id)
-        VALUES ($1, $2, $3, $4) RETURNING id
-    `, user.FirstName, user.LastName, user.Age, user.CarID).Scan(&id)
+        INSERT INTO data.users (first_name, last_name, age)
+        VALUES ($1, $2, $3) RETURNING id
+    `, user.FirstName, user.LastName, user.Age).Scan(&id)
 
 	return id, err
 }
